dd11abc68375f23e1c87f6ff935f5810d0656700
unknown <ninadmaadhavi13@gmail.com>
2024-02-17 13:57:56 +0530

    Phase component added

---
+++ src/components/PhaseList.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const PhaseList = () => {
+  return (
+    <div className='flex flex-col justify-center'>
+        <div className='min-w-3 my-3'>Phase 1</div>
+        <div className='min-w-3 my-3'>Phase 2</div>
+        <div className='min-w-3 my-3'>Phase 3</div>
+    </div>
+  )
+}
+
+export default PhaseList
