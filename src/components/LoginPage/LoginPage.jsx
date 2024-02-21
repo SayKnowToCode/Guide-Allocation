3a3fbb75b5adac26777346cd78038fe798c9e746
AK-47Code <atharvakhabale2004@gmail.com>
2024-02-21 22:28:00 +0530

    Register Page Created

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -2,71 +2,79 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-
+import './LoginPage.css';
 const LoginForm = () => {
-  const [teamName, setTeamName] = useState('');
-  const [password, setPassword] = useState('');
-  const navigate = useNavigate();
+    const [teamName, setTeamName] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    // Handle form submission logic here
-    try {
-        const response = await axios.post('http://localhost:3500/login', {
-            teamName,
-            password
-        })
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        // Handle form submission logic here
+        try {
+            const response = await axios.post('http://localhost:3500/login', {
+                teamName,
+                password
+            })
 
-        localStorage.setItem('teamData',JSON.stringify(response.data))
-        navigate('/studentDashboard')
-    }
-    catch (err)
-    {
-        console.log(err.response.data);
-    }
-  };
+            localStorage.setItem('teamData', JSON.stringify(response.data))
+            navigate('/studentDashboard')
+        }
+        catch (err) {
+            console.log(err.response.data);
+        }
+    };
 
-  return (
-    <div className="form-container text-white">
-      <p className='h2'>Login</p>
-      <form className="form" onSubmit={handleSubmit}>
-        <div className="input-group">
-          <label htmlFor="teamName">Teamname</label>
-          <input
-            type="text"
-            name="teamName"
-            id="teamName"
-            placeholder=""
-            value={teamName}
-            onChange={(e) => setTeamName(e.target.value)}
-          />
-        </div>
-        <div className="input-group">
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            placeholder=""
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <div className="forgot">
-            <p>
-              <Link to='/register'>Forgot Password?</Link>
+    return (
+        <div className="form-container text-white">
+            <p className='h2'>Login</p>
+            <div className='option flex gap-16'>
+                <button className='button'>
+                    <span>Student</span>
+                </button>
+                <button className='button'>
+                    <span>Faculty</span>
+                </button>
+            </div>
+            <form className="form" onSubmit={handleSubmit}>
+                <div className="inputContainer">
+                    <input
+                        type="text"
+                        name="teamName"
+                        id="inputField"
+                        placeholder=""
+                        value={teamName}
+                        onChange={(e) => setTeamName(e.target.value)}
+
+                    />
+                    <label className='usernameLabel' htmlFor="inputField">Teamname</label>
+                </div>
+                <div className="inputContainer">
+                    <input
+                        type="password"
+                        name="password"
+                        id="inputField"
+                        placeholder=""
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <label className="usernameLabel" htmlFor="inputField">Password</label>
+                    <div className="forgot">
+                        <p>
+                            <Link to='/register'>Forgot Password?</Link>
+                        </p>
+                    </div>
+                </div>
+                <div>
+                    <button type="submit" className="sign">Log in</button>
+                </div>
+            </form>
+            <p className="signup">
+                Don't have an account?
+                <Link className="Register" to='/register'>Register</Link>
             </p>
-          </div>
         </div>
-        <div>
-          <button type="submit" className="sign">Sign in</button>
-        </div>
-      </form>
-      <p className="signup">
-        Don't have an account?
-        <Link to='/register'>Sign up</Link>
-      </p>
-    </div>
-  );
+    );
 };
 
 export default LoginForm;
--- src/components/RegisterPage/RegisterPage.jsx
+++ src/components/RegisterPage/RegisterPage.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router';
 import axios from 'axios';
+import './RegisterPage.css'
+import './Extra.css'
 
 const RegisterPage = () => {
   const navigate = useNavigate();
@@ -28,183 +30,206 @@ const RegisterPage = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    
-    if(formData.password === formData.confirmPassword)
-    {
-      try{
-        const response = await axios.post('http://localhost:3500/register',{
-          teamName : formData.teamName,
-          password : formData.password,
-          leaderName : formData.leaderName,
-          leaderUID : formData.leaderUID,
-          leaderEmail : formData.leaderEmail,
-          member1Name : formData.member1Name, 
-          member1UID : formData.member1UID,  
-          member1Email : formData.member1Email,
-          member2Name : formData.member2Name, 
-          member2UID : formData.member2UID, 
-          member2Email : formData.member2Email 
+
+    if (formData.password === formData.confirmPassword) {
+      try {
+        const response = await axios.post('http://localhost:3500/register', {
+          teamName: formData.teamName,
+          password: formData.password,
+          leaderName: formData.leaderName,
+          leaderUID: formData.leaderUID,
+          leaderEmail: formData.leaderEmail,
+          member1Name: formData.member1Name,
+          member1UID: formData.member1UID,
+          member1Email: formData.member1Email,
+          member2Name: formData.member2Name,
+          member2UID: formData.member2UID,
+          member2Email: formData.member2Email
         })
         console.log(response.data);
         navigate('/');
       }
-      catch(err)
-      {
+      catch (err) {
         console.log(err.response.data);
       }
-      
+
     }
     else
       console.log("Password and Confirm Password do not match");
-    
+
   };
 
   return (
-    <div className='text-white'>
-      <h2>Team Registration</h2>
-      <br />
-      <br />
-      <form onSubmit={handleSubmit}>
+    <div className='text-white Register-Container'>
+      <div className='home-title'>
+        <span><p>Welcome To The Site</p></span>
+      </div>
+      <h2 className='Heading'>Team Registration</h2>
+      <form className='Form' onSubmit={handleSubmit}>
         {/* Team Name */}
-        <label htmlFor="teamName">Team Name:</label>
-        <input
-          type="text"
-          id="teamName"
-          name="teamName"
-          value={formData.teamName}
-          onChange={handleChange}
-          required
-        />
-        <br />
-        <br />
+        <div className='inputGroup'>
+          <input
+            type="text"
+            id="teamName"
+            name="teamName"
+            value={formData.teamName}
+            onChange={handleChange}
+            required
+            autoComplete='off'
+          />
+          <label htmlFor="teamName">Team Name:</label>
+        </div>
         {/* Leader Information */}
-        <label htmlFor="leaderName">Leader Name:</label>
-        <input
-          type="text"
-          id="leaderName"
-          name="leaderName"
-          value={formData.leaderName}
-          onChange={handleChange}
-          required
-        />
-        <br />
-        <br />
-        <label htmlFor="leaderUID">Leader UID:</label>
-        <input
-          type="text"
-          id="leaderUID"
-          name="leaderUID"
-          value={formData.leaderUID}
-          onChange={handleChange}
-          required
-        />
-        <br />
-        <br />
-        <label htmlFor="leaderEmail">Leader SPIT Email:</label>
-        <input
-          type="email"
-          id="leaderEmail"
-          name="leaderEmail"
-          value={formData.leaderEmail}
-          onChange={handleChange}
-          required
-        />
-        <br />
-        <br />
-        {/* Member 1 Information */}
-        <label htmlFor="member1Name">Member 1 Name:</label>
-        <input
-          type="text"
-          id="member1Name"
-          name="member1Name"
-          value={formData.member1Name}
-          onChange={handleChange}
-          required
-        />
-        <br />
-        <br />
-        <label htmlFor="member1UID">Member 1 UID:</label>
-        <input
-          type="text"
-          id="member1UID"
-          name="member1UID"
-          value={formData.member1UID}
-          onChange={handleChange}
-          required
-        />
-        <br />
-        <br />
-        <label htmlFor="member1Email">Member 1 SPIT Email:</label>
-        <input
-          type="email"
-          id="member1Email"
-          name="member1Email"
-          value={formData.member1Email}
-          onChange={handleChange}
-          required
-        />
-        <br />
-        <br />
-        {/* Member 2 Information */}
-        <label htmlFor="member2Name">Member 2 Name:</label>
-        <input
-          type="text"
-          id="member2Name"
-          name="member2Name"
-          value={formData.member2Name}
-          onChange={handleChange}
-          required
-        />
-        <br />
-        <br />
-        <label htmlFor="member2UID">Member 2 UID:</label>
-        <input
-          type="text"
-          id="member2UID"
-          name="member2UID"
-          value={formData.member2UID}
-          onChange={handleChange}
-          required
-        />
-        <br />
-        <br />
-        <label htmlFor="member2Email">Member 2 SPIT Email:</label>
-        <input
-          type="email"
-          id="member2Email"
-          name="member2Email"
-          value={formData.member2Email}
-          onChange={handleChange}
-          required
-        />
-        <br />
-        <br />
-        {/* Password */}
-        <label htmlFor="password">Set Password:</label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          value={formData.password}
-          onChange={handleChange}
-          required
-        />
-        <br />
-        <br />
-        {/* Confirm Password */}
-        <label htmlFor="confirmPassword">Confirm Password:</label>
-        <input
-          type="password"
-          id="confirmPassword"
-          name="confirmPassword"
-          value={formData.confirmPassword}
-          onChange={handleChange}
-          required
-        />
-        <br />
-        <br />
-        {/* Submit Button */}
-        <button type="submit">Register</button>
+        <div className='teamName'>
+          <div className='inputGroup'>
+            <input
+              type="text"
+              id="leaderName"
+              name="leaderName"
+              value={formData.leaderName}
+              onChange={handleChange}
+              required
+              autoComplete='off'
+            />
+            <label htmlFor="leaderName">Leader Name:</label>
+          </div>
+          <div className='inputGroup'>
+            <input
+              type="text"
+              id="leaderUID"
+              name="leaderUID"
+              value={formData.leaderUID}
+              onChange={handleChange}
+              required
+              autoComplete='off'
+            />
+            <label htmlFor="leaderUID">Leader UID:</label>
+          </div>
+          <div className='inputGroup'>
+            <input
+              type="email"
+              id="leaderEmail"
+              name="leaderEmail"
+              value={formData.leaderEmail}
+              onChange={handleChange}
+              required
+              autoComplete='off'
+            />
+            <label htmlFor="leaderEmail">Leader SPIT Email:</label>
+          </div>
+        </div>
+        <div className='teamName'>
+          {/* Member 1 Information */}
+          <div className='inputGroup'>
+            <input
+              type="text"
+              id="member1Name"
+              name="member1Name"
+              value={formData.member1Name}
+              onChange={handleChange}
+              required
+              autoComplete='off'
+            />
+            <label htmlFor="member1Name">Member 1 Name:</label>
+          </div>
+          <div className='inputGroup'>
+            <input
+              type="text"
+              id="member1UID"
+              name="member1UID"
+              value={formData.member1UID}
+              onChange={handleChange}
+              required
+              autoComplete='off'
+            />
+            <label htmlFor="member1UID">Member 1 UID:</label>
+          </div>
+          <div className='inputGroup'>
+            <input
+              type="email"
+              id="member1Email"
+              name="member1Email"
+              value={formData.member1Email}
+              onChange={handleChange}
+              required
+              autoComplete='off'
+            />
+            <label htmlFor="member1Email">Member 1 SPIT Email:</label>
+          </div>
+        </div>
+        <div className='teamName'>
+          {/* Member 2 Information */}
+          <div className='inputGroup'>
+            <input
+              type="text"
+              id="member2Name"
+              name="member2Name"
+              value={formData.member2Name}
+              onChange={handleChange}
+              required
+              autoComplete='off'
+            />
+            <label htmlFor="member2Name">Member 2 Name:</label>
+            </div>
+            <div className='inputGroup'>
+              <input
+                type="text"
+                id="member2UID"
+                name="member2UID"
+                value={formData.member2UID} 
+                onChange={handleChange}
+                required
+                autoComplete='off'
+              />
+              <label htmlFor="member2UID">Member 2 UID:</label>
+            </div>
+            <div className='inputGroup'>
+              <input
+                type="email"
+                id="member2Email"
+                name="member2Email"
+                value={formData.member2Email}
+                onChange={handleChange}
+                required
+                autoComplete='off'
+              />
+              <label htmlFor="member2Email">Member 2 SPIT Email:</label>
+            </div>
+          </div>
+          {/* Password */}
+          <div className='teamName'>
+            <div className='inputGroup'>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                required
+                autoComplete='off'
+              />
+              <label htmlFor="password">Set Password:</label>
+            </div>
+          </div>
+          <div className='teamName'>
+            {/* Confirm Password */}
+            <div className='inputGroup'>
+              <input
+                type="password"
+                id="confirmPassword"
+                name="confirmPassword"
+                value={formData.confirmPassword}
+                onChange={handleChange}
+                required
+                autoComplete='off'
+              />
+              <label htmlFor="confirmPassword">Confirm Password:</label>
+            </div>
+          </div>
+          <div className='teamName ml-48'>
+            {/* Submit Button */}
+            <button className="reg" type="submit">Register</button>
+          </div>
       </form>
     </div>
   );
